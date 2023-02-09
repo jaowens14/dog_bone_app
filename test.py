8fc299fffffe627bb01ef3ebbea3a4442ef8b1c8
jowens4 <jowens@avidpd.com>
2023-02-09 12:44:34 -0700

    tseting html parser

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
 import pandas as pd
 import numpy as np
+from bs4 import BeautifulSoup
 
-test_data = pd.read_csv("test_file.csv").to_numpy()
+html_file = open("02092023-01.html")
 
-s = np.max(test_data[:,0])
-p = np.max(test_data[:,1])
-print(s,p)
+index = html_file.read()
+
+soup = BeautifulSoup(index, 'html.parser')
+table = soup.find_all("table")
+
+#table = table.get_text().split("\n\n")
+
+print(table)
+
+text = soup.get_text().splitlines()
+for section in text:
+    if "BUILD" in section:
+        print(section)
 
 
+# git add .
+# git commit -m "commit here"
+# git push -u origin master
